a802a3fddb3a0023aaf5e8874f5921fc2e0e61b4
Grant Murphy <gcmurphy@pm.me>
2023-02-27 13:47:54 +1000

    refactor: use features for schema and client
    The client module now contains only code for hitting the osv API
    endpoint.
    
    The rust bindings to the osv schema are now the only thing enabled
    by default and now live in the schema module.
    
    fixes: #7.

--- examples/parse.rs
+++ examples/parse.rs
@@ -2,8 +2,10 @@ use std::env;
 use std::fs;
 use std::process;
 
+use osv::client;
+
 #[async_std::main]
-async fn main() -> Result<(), osv::ApiError> {
+async fn main() -> Result<(), client::ApiError> {
     let args: Vec<String> = env::args().skip(1).collect();
     if args.len() <= 0 {
         println!("filename expected");
@@ -12,7 +14,7 @@ async fn main() -> Result<(), osv::ApiError> {
     for arg in &args {
         let path: &str = arg.as_str();
         let file = fs::File::open(path).unwrap();
-        let _vuln: osv::Vulnerability =
+        let _vuln: osv::schema::Vulnerability =
             serde_json::from_reader(file).unwrap_or_else(|_| panic!("fail: {}", path));
         println!("pass: {}", path);
     }
